98589df6793705d4a9fcc47037862e7e9e374e39
Mike K <m.kermes89@gmail.com>
2018-05-19 16:02:48 -0700

    complete object literals. working on summing hourly totals with reduce function

---
+++ js/app.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+
+//random function
+/* function randomCustomers() {
+  return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + 1) + this.minCustomer;
+};
+*/
+
+// object literal for Pike Location
+var pikeStore = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgSales: 6.3,
+  hourlyTotal: [],
+  getCustomers: function(){
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) +1) + this.minCustomer;
+  },
+
+  hourlySales: function(){
+    for(var i = 0; i < storeHours.length; i++){
+      var customersPerHour = this.getCustomers();
+      var salesPerHour = Math.floor(customersPerHour * this.avgSales);
+      this.hourlyTotal.push(salesPerHour);
+    }
+  },
+};
+pikeStore.hourlySales();
+
+var pikeSales = pikeStore.hourlyTotal.reduce((acc, val) => {
+  return acc + val;
+});
+console.log('total', pikeSales);
+
+var seatacStore = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgSales: 1.23,
+  totalSales: 0,
+  hourlyTotal: [],
+  getCustomers: function(){
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) +1) + this.minCustomer;
+  },
+
+  hourlySales: function(){
+    for(var i = 0; i < storeHours.length; i++){
+      var customersPerHour = this.getCustomers();
+      var salesPerHour = Math.floor(customersPerHour * this.avgSales);
+      this.hourlyTotal.push(salesPerHour);
+    }
+  }
+};
+seatacStore.hourlySales();
+var seatacSales = seatacStore.hourlyTotal.reduce((acc, val) => {
+  return acc + val;
+});
+console.log(seatacStore.hourlyTotal);
+
+var seattleCenterStore = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgSales: 3.7,
+  totalSales: 0,
+  hourlyTotal: [],
+  getCustomers: function(){
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) +1) + this.minCustomer;
+  },
+
+  hourlySales: function(){
+    for(var i = 0; i < storeHours.length; i++){
+      var customersPerHour = this.getCustomers();
+      var salesPerHour = Math.floor(customersPerHour * this.avgSales);
+      this.hourlyTotal.push(salesPerHour);
+    }
+  }
+};
+seattleCenterStore.hourlySales();
+var seattleCenterSales = seattleCenterStore.hourlyTotal.reduce((acc, val) => {
+  return acc + val;
+});
+console.log(seattleCenterStore.hourlyTotal);
+
+var capHillStore = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgSales: 2.3,
+  totalSales: 0,
+  hourlyTotal: [],
+  getCustomers: function(){
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) +1) + this.minCustomer;
+  },
+
+  hourlySales: function(){
+    for(var i = 0; i < storeHours.length; i++){
+      var customersPerHour = this.getCustomers();
+      var salesPerHour = Math.floor(customersPerHour * this.avgSales);
+      this.hourlyTotal.push(salesPerHour);
+    }
+  }
+};
+capHillStore.hourlySales();
+console.log(capHillStore.hourlyTotal);
+
+var alkiStore = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgSales: 4.6,
+  totalSales: 0,
+  hourlyTotal: [],
+  getCustomers: function(){
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) +1) + this.minCustomer;
+  },
+
+  hourlySales: function(){
+    for(var i = 0; i < storeHours.length; i++){
+      var customersPerHour = this.getCustomers();
+      var salesPerHour = Math.floor(customersPerHour * this.avgSales);
+      this.hourlyTotal.push(salesPerHour);
+    }
+  }
+};
+alkiStore.hourlySales();
+console.log(alkiStore.hourlyTotal);
